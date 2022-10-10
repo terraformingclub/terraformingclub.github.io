b69cfb59eef3db2852fe376cf0b607229281c163
Arturo Anguix <arturo.anguix.l@gmail.com>
2022-10-11 00:36:45 +0200

    prehashbullah

--- src/App.js
+++ src/App.js
@@ -1,17 +1,32 @@
 import logo from './logo.svg';
 import './App.css';
 import Resource from './components/resource/resource';
+import useScreenOrientation from 'react-hook-screen-orientation'
+import { Fragment } from 'react';
 
 function App() {
-  return (
+  const screenOrientation = useScreenOrientation();
+  let content = null
+
+  if (screenOrientation === "landscape-primary" || screenOrientation === "landscape-secondary") {
+    content = 
     <div className="App">
-      <Resource resourceClassName="resource-megacredit"></Resource>
-      <Resource resourceClassName="resource-steel"></Resource>
-      <Resource resourceClassName="resource-titanium"></Resource>
-      <Resource resourceClassName="resource-plants"></Resource>
-      <Resource resourceClassName="resource-energy"></Resource>
-      <Resource resourceClassName="resource-heat"></Resource>
+      <Resource resourceClassName="resource-megacredit" top="8vh" left={15} limit={-5}></Resource>
+      <Resource resourceClassName="resource-steel" top="8vh" left={48} limit={0}></Resource>
+      <Resource resourceClassName="resource-titanium" top="8vh" left={82} limit={0}></Resource>
+      <Resource resourceClassName="resource-plants" top="45vh" left={15} limit={0}></Resource>
+      <Resource resourceClassName="resource-energy" top="45vh" left={48} limit={0}></Resource>
+      <Resource resourceClassName="resource-heat" top="45vh" left={82} limit={0}></Resource>
     </div>
+  }else {
+    content = 
+    <div className="portrait-div">
+      Tarn araun plis
+    </div>
+  }
+
+  return (
+    <Fragment>{content}</Fragment>
   );
 }
 
--- src/components/resource/resource.js
+++ src/components/resource/resource.js
@@ -1,19 +1,21 @@
 import React, { useState } from 'react';
 import './resource.css'
 
-import resourcesTable from './../../images/resources-table.jpg'
 const Resource=(props)=> {
+
   const [value, setValue] = useState(0);   
   const classes = `resource-div  ${props.resourceClassName}`
+  const leftOffset = value < 0 || value > 9 ? -3 : 0
+  let left = props.left + leftOffset
       return (
         <div className={classes}>
-        <button className='minus' onClick = {() => setValue(value -1)}>
+        <button className='minus' onClick = {() => setValue(value <= props.limit ? props.limit : value - 1)}>
           -
         </button>
         <button className='plus' onClick = {() => setValue(value + 1)}>
           +
         </button>
-        <div className='resource-value'>{value}</div>
+        <div className='resource-value' style={{top : `${props.top}`, left : `${left}%`}}>{value}</div>
         </div>
         );
   }
